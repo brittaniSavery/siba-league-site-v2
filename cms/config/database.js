98c7e7152642abb02a87d9089b3740cbc89b6db7
Brittani S Avery <brittani.s.avery@gmail.com>
2022-10-26 23:31:09 -0500

    update config folder

--- cms/config/database.js
+++ cms/config/database.js
@@ -1,18 +1,11 @@
+const path = require('path');
+
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "siba-cms"),
-        username: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "admin"),
-        ssl: false,
-      },
-      options: {},
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
+    useNullAsDefault: true,
   },
 });
