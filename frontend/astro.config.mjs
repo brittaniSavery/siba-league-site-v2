7ae337423eabcad5a0cc2cd2f4e8fbe66795e1dd
Brittani S Avery <brittani.s.avery@gmail.com>
2022-12-02 22:42:20 -0600

    Change styling for pro definition list

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import {
+  remarkDefinitionList,
+  defListHastHandlers,
+} from "remark-definition-list";
 
 // https://astro.build/config
 import react from "@astrojs/react";
@@ -7,5 +11,10 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx()],
+  markdown: {
+    extendDefaultPlugins: true,
+    remarkPlugins: [remarkDefinitionList],
+    remarkRehype: { handlers: { ...defListHastHandlers } },
+  },
   site: "https://siba.averyincorporated.com",
 });
