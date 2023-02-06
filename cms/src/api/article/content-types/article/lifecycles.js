aa203d1b3b0edc1d6f6d6b26f22fa1d9f68968e8
Brittani S Avery <brittani.s.avery@gmail.com>
2023-02-06 14:49:11 -0600

    Redo 'Author Required' Error for strapi v4

--- cms/src/api/article/content-types/article/lifecycles.js
+++ cms/src/api/article/content-types/article/lifecycles.js
@@ -1,18 +1,28 @@
+const utils = require("@strapi/utils");
+const { ApplicationError } = utils.errors;
+
 module.exports = {
   async beforeUpdate(event) {
     const { data: updated, where, populate } = event.params;
 
+    // getting the current version of the article
     const current = await strapi
       .service("api::article.article")
       .findOne(where.id);
     const isPublished = updated.publishedAt || current.publishedAt;
 
-    console.log(current);
+    console.log("data", updated);
+    console.log("populate", populate);
 
-    if (isPublished && !(updated.author || current.author)) {
-      throw strapi.errors.badRequest(
-        "Author is required. Please select the author on the right-hand panel."
-      );
+    // double-checking that there is an author for published articles
+
+    if (isPublished) {
+      const authorRemoved = updated.author.disconnect.length > 0;
+      const authorAdded = updated.author.connect.length > 0;
+
+      if (!populate.author.count || (authorRemoved && !authorAdded)) {
+        throw new ApplicationError("Author is required.");
+      }
     }
   },
 };
