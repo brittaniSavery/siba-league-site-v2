4e3b1160f78deb6ace70b7756788d3c26ca4db91
Brittani S Avery <brittani.s.avery@gmail.com>
2023-02-09 19:21:21 -0600

    Add frontend rebuild trigger on strapi updates

---
+++ cms/src/api/article/content-types/article/lifecycles.js
@@ -0,0 +1,54 @@
+const functions = require("../../../../index");
+
+const utils = require("@strapi/utils");
+const { ApplicationError } = utils.errors;
+
+module.exports = {
+  async beforeUpdate(event) {
+    const { data: updated, where, populate } = event.params;
+
+    // getting the current version of the article
+    const current = await strapi
+      .service("api::article.article")
+      .findOne(where.id, {
+        fields: ["publishedAt"],
+        populate: { author: true },
+      });
+
+    console.log({ current });
+
+    const isPublished = updated.publishedAt || current.publishedAt;
+
+    // double-checking that there is an author for published articles
+
+    if (!!isPublished) {
+      const authorRemoved = updated.author?.disconnect.length > 0;
+      const authorAdded = updated.author?.connect.length > 0;
+
+      if (!current.author || (authorRemoved && !authorAdded)) {
+        throw new ApplicationError("Author is required.");
+      }
+    }
+  },
+
+  async afterUpdate(event) {
+    const { where } = event.params;
+    console.dir(event.result.publishedAt);
+
+    // getting the current version of the article
+    const current = await strapi
+      .service("api::article.article")
+      .findOne(where.id, {
+        fields: ["publishedAt"],
+      });
+
+    // article is currently unpublished, so no need to trigger rebuild
+    if (!current.publishedAt) return;
+
+    if (process.env.NODE_ENV === "development") {
+      strapi.log.debug("Triggered rebuild on Github Actions");
+    } else {
+      strapi.service("api::article.article").triggerRebuild();
+    }
+  },
+};
--- cms/src/api/article/services/article.js
+++ cms/src/api/article/services/article.js
@@ -1,9 +1,37 @@
-'use strict';
+// @ts-nocheck
+"use strict";
 
 /**
  * article service
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService('api::article.article');
+module.exports = createCoreService("api::article.article", ({ strapi }) => ({
+  async triggerRebuild() {
+    strapi.log.info("Starting github action trigger");
+    const fetch = (...args) =>
+      import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+    const response = await fetch(
+      "https://api.github.com/repos/brittanisavery/siba-league-site-v2/dispatches",
+      {
+        method: "POST",
+        body: JSON.stringify({ event_type: "strapi-update" }),
+        headers: {
+          Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+          Accept: "application/vnd.github+json",
+        },
+      }
+    );
+
+    if (response.ok) {
+      strapi.log.info("Triggered build/deploy for frontend website.");
+    } else {
+      const error = await response.json();
+      strapi.log.warn(
+        `An error occurred when trying to contact Github: ${error.message}. Get more information at ${error.documentation_url}`
+      );
+    }
+  },
+}));
