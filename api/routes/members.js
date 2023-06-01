001f456854803648b905af6eed399e9e2041c0e6
Brittani S Avery <brittani.s.avery@gmail.com>
2023-06-01 00:41:08 -0500

    Move fetchQuery function to utils

--- api/routes/members.js
+++ api/routes/members.js
@@ -1,5 +1,5 @@
-import mysql from "mysql";
 import { Router } from "express";
+import { fetchQuery } from "../utils.js";
 
 const router = Router();
 
@@ -10,40 +10,15 @@ router.get("/uploads", async (req, res, next) => {
   if (!league || !["college", "pro"].includes(league)) {
     res
       .status(400)
-      .send("The query league is required and must be 'pro' or 'college'.");
+      .send(
+        'The query parameter "league" is required and must be "pro" or "college".'
+      );
     return;
   }
 
-  const cmsResponse = await fetch(`${process.env.CMS_URL}/${league}-info`);
+  const uploads = {};
 
-  if (!cmsResponse.ok) throw new Error(cmsResponse.statusText);
-
-  const leagueInfo = await cmsResponse.json();
-  const teams = leagueInfo.data.attributes.teams;
-
-  console.log(teams);
-
-  const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: "averyinc_siba",
-  });
-
-  connection.connect();
-
-  connection.query(
-    `SELECT * FROM ${league}_team_uploads ORDER BY teamID`,
-    function (error, results) {
-      if (error) {
-        next(error);
-      }
-
-      console.log(results);
-    }
-  );
-
-  connection.end();
+  await fetchQuery(`SELECT * FROM ${league}_team_uploads ORDER BY teamID`);
 
   res.end();
 });
---
+++ api/utils.js
@@ -0,0 +1,14 @@
+import mysql from "mysql2/promise";
+
+export async function fetchQuery(query, params) {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "averyinc_siba",
+  });
+
+  const [rows, fields] = await connection.execute(query, params || []);
+
+  console.log(rows);
+}
