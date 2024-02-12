530dd63512bff63b5c1014eeb0d0b0a1b9ca9a79
Brittani S Avery <brittani.s.avery@gmail.com>
2024-02-12 09:57:50 -0600

    Start of tabs for pro/college teams

--- frontend/src/components/JoinForm2/TeamPicks.tsx
+++ frontend/src/components/JoinForm2/TeamPicks.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
-import type { CollegeForm, ProForm } from "./lib";
+import type { MainForm } from "./lib";
 import clsx from "clsx";
+import { useFormContext } from "react-hook-form";
 
 export default function TeamPicks() {
   const [tabView, setTabView] = useState<"pro" | "college">("pro");
+  const methods = useFormContext<MainForm>();
 
   return (
-    <>
+    <div className="pb-4">
       <div className="content mt-5">
         <h2>Pick Your Teams</h2>
         <p>
@@ -17,20 +19,29 @@ export default function TeamPicks() {
         </p>
       </div>
 
-      <div className="card is-shadowless"></div>
-      <div className="tabs is-boxed">
-        <ul>
-          <li className={clsx(tabView === "pro" && "is-active")}>
-            <a onClick={() => setTabView("pro")}>Pro</a>
-          </li>
-          <li className={clsx(tabView === "college" && "is-active")}>
-            <button onClick={() => setTabView("college")}>College</button>
-          </li>
-        </ul>
+      <div className="card is-shadowless">
+        <div className="has-background-light pt-2 px-2 mb-0 tabs is-boxed">
+          <ul>
+            <li className={clsx(tabView === "pro" && "is-active")}>
+              <a onClick={() => setTabView("pro")}>Pro</a>
+            </li>
+            <li className={clsx(tabView === "college" && "is-active")}>
+              <a onClick={() => setTabView("college")}>College</a>
+            </li>
+          </ul>
+        </div>
+        <div className="card-content">
+          <div className={clsx(tabView !== "pro" && "is-hidden")}>
+            <p>You are able to manage only one (1) pro team.</p>
+          </div>
+          <div className={clsx(tabView !== "college" && "is-hidden")}>
+            <p>
+              You are allowed to coach up to three (3) teams. They each must be
+              in different tiers and different recruiting regions.
+            </p>
+          </div>
+        </div>
       </div>
-      <div className="card-header"></div>
-
-      <div className="card-content"></div>
-    </>
+    </div>
   );
 }
