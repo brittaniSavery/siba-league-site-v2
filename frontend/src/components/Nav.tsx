53f0ec6a40f28087ae5a81977cd0c902be8509a0
Brittani S Avery <brittani.s.avery@gmail.com>
2025-02-25 21:57:19 -0600

    Update bulma css + move old pages

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -64,7 +64,7 @@ export default function Nav() {
 
   return (
     <nav
-      className="navbar is-fixed-top is-light"
+      className="navbar is-fixed-top"
       role="navigation"
       aria-label="main navigation"
     >
