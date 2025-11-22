3e05a8d1fcbfeefa9a44d8fb4948355c1d05d20c
Brittani S Avery <brittani.s.avery@gmail.com>
2025-11-21 19:17:25 -0600

    Add events to college calendar

---
+++ frontend/src/components/RulesPage/CollegeCalender.tsx
@@ -0,0 +1,25 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import collegeEvents from "@assets/college-events.json";
+import "@styles/college-calendar.scss";
+
+export default function CollegeCalendar() {
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      initialDate="2022-05-01"
+      titleFormat={{ month: "long" }}
+      validRange={{ start: "2022-05-01", end: "2023-04-30" }}
+      navLinks={false}
+      defaultAllDay={true}
+      events={collegeEvents}
+      eventOrder="order,start,-duration,title"
+      headerToolbar={{
+        left: "prev",
+        center: "title",
+        right: "next",
+      }}
+    />
+  );
+}
